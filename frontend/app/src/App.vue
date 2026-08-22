50ab86598af2e96e8590505e135f98436647877a
zhaoysg <39149223+zhaoysg@users.noreply.github.com>
2026-08-22 10:18:00 +0800

    refactor(app): load global brand theme from App

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,7 +1,15 @@
 <script setup lang="ts">
 import { onLaunch } from "@dcloudio/uni-app";
 
-onLaunch(() => console.info("财不外露用户端启动"));
+onLaunch(() => {
+  console.info("财不外露用户端启动");
+});
 </script>
 
-<template><slot /></template>
+<template>
+  <slot />
+</template>
+
+<style lang="scss">
+@import "./styles/theme.scss";
+</style>
